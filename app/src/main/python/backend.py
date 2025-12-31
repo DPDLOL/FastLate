77799cbcf433aaf98573b1fa639bfbc7f10d1db1
DPDLOL <debdwaipayan250@gmail.com>
2025-12-31 19:43:34 +0530

    brrr go translations

---
+++ app/src/main/python/backend.py
@@ -0,0 +1,14 @@
+def start_pairing():
+    return {
+        "status": "Paired",
+        "device_name": "ESP32-FastLate",
+        "mac": "24:6F:28:AB:3C:91",
+        "connection": "Bluetooth"
+    }
+
+
+def start_listening():
+    return {
+        "status": "Listening",
+        "latency": 120
+    }
